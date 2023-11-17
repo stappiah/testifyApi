6bc13c93d84f9c309584523473f6fa29b2ef0594
Stephen Appiah <stappiah@deloitte.com.gh>
2023-11-17 09:23:00 +0000

    account model updated

--- account/models.py
+++ account/models.py
@@ -15,6 +15,7 @@ class AccountManager(BaseUserManager):
 
     def create_superuser(self, email, password=None, **extra_fields):
         extra_fields.setdefault("is_staff", True)
+        extra_fields.setdefault("is_active", True)
         extra_fields.setdefault("is_superuser", True)
 
         if extra_fields.get("is_staff") is not True:
@@ -32,6 +33,7 @@ class Account(AbstractBaseUser, PermissionsMixin):
     phone_number = models.CharField(max_length=20, null=True, blank=True)
     is_active = models.BooleanField(default=True)
     is_staff = models.BooleanField(default=False)
+    is_superuser = models.BooleanField(default=False)
     profile_image = models.ImageField(upload_to='user_images/', null=True, blank=True)
     date_joined = models.DateTimeField(auto_now_add=True)
     date_updated = models.DateTimeField(auto_now=True)
