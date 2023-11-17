e27c498affe7f5ab604c155917d6bf96a3e662d8
Stephen Appiah <stappiah@deloitte.com.gh>
2023-11-17 00:03:42 +0000

    URL updated

--- testifyApi/settings.py
+++ testifyApi/settings.py
@@ -22,7 +22,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = "django-insecure-y$llebw08-j$_od)ft^pb1i2##$%$57(+$yd@ik2jb8%*yt2&^"
+SECRET_KEY = os.environ.get('SECRET_KEY', "django-insecure-y$llebw08-j$_od)ft^pb1i2##$%$57(+$yd@ik2jb8%*yt2&^")
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = os.environ.get('DEBUG', 'True')=="True"
--- testifyApi/urls.py
+++ testifyApi/urls.py
@@ -2,7 +2,6 @@ from django.contrib import admin
 from django.urls import path, include
 from django.conf import settings
 from django.conf.urls.static import static
-from django.contrib.staticfiles.urls import staticfiles_urlpatterns
 
 urlpatterns = [
     path('admin/', admin.site.urls),
@@ -11,4 +10,3 @@ urlpatterns = [
     path('vendor/', include('vendor.urls')),
 ]
 urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
-# urlpatterns += staticfiles_urlpatterns()
