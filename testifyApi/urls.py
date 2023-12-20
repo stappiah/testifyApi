3a91d4bc580da73e0819a5a7b78fa6fdfbd26a8c
Stephen Appiah <stappiah@deloitte.com.gh>
2023-12-20 17:02:02 +0000

    url changed

--- testifyApi/urls.py
+++ testifyApi/urls.py
@@ -9,4 +9,5 @@ urlpatterns = [
     path('account/', include('account.urls')),
     path('vendor/', include('vendor.urls')),
 ]
-urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+if settings.DEBUG:
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
